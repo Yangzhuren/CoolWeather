ec6e2aacec88966ff837353df958b0a5853c4f05
Yangxian <570486455@qq.com>
2018-02-05 13:23:50 +0800

    添加数据库和表的各项配置

---
+++ app/src/main/java/com/yangxian/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.yangxian.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by MZyx on 2018/2/5.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityname() {
+        return cityName;
+    }
+
+    public void setCityname(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
